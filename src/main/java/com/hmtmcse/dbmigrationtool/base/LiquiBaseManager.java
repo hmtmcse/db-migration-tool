ddcef86f230cbdfd05e0a1873d6ff2430251c28d
Touhid Mia <hmtm.cse@gmail.com>
2019-02-27 00:20:01 +0600

    Liquibase

--- src/main/java/com/hmtmcse/dbmigrationtool/base/LiquiBaseManager.java
+++ src/main/java/com/hmtmcse/dbmigrationtool/base/LiquiBaseManager.java
@@ -5,17 +5,26 @@ import liquibase.integration.commandline.Main;
 
 public class LiquiBaseManager {
 
-
-
-    public void start(){
-        String[] myList = {
+    public static void main(String[] args) {
+        String[] generateChangeLog = {
                 "--changeLogFile=current-changelog.xml",
                 "--url=jdbc:mysql://localhost/task_manager",
                 "--username=root",
                 "--password=",
-                "generateChangeLog"};
+                "generateChangeLog"
+        };
 
-        Main.main(myList);
+        String[] dbDoc = {
+                "--changeLogFile=current-changelog.xml",
+                "--url=jdbc:mysql://localhost/task_manager",
+                "--username=root",
+                "--password=",
+                "dbDoc",
+                "./dbDoc",
+        };
+
+
+        Main.main(dbDoc);
     }
 
 }
