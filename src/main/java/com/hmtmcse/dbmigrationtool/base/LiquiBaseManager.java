ca8473b68bb87d77aa01b8112ccc61302acfb5b5
Touhid Mia <hmtm.cse@gmail.com>
2020-02-16 21:28:46 +0600

    Added Liquibase JSON form

--- src/main/java/com/hmtmcse/dbmigrationtool/base/LiquiBaseManager.java
+++ src/main/java/com/hmtmcse/dbmigrationtool/base/LiquiBaseManager.java
@@ -7,7 +7,7 @@ public class LiquiBaseManager {
 
     public static void main(String[] args) {
         String[] generateChangeLog = {
-                "--changeLogFile=current-changelog.xml",
+                "--changeLogFile=current-changelog.json",
                 "--url=jdbc:mysql://localhost/task_manager",
                 "--username=root",
                 "--password=",
@@ -23,8 +23,38 @@ public class LiquiBaseManager {
                 "./dbDoc",
         };
 
+        String[] updateSQL = {
+                "--changeLogFile=current-changelog.xml",
+                "--url=jdbc:mysql://localhost/task_manager1",
+                "--username=root",
+                "--password=",
+                "--outputFile=./dbDoc/updateSql.sql",
+                "updateSQL"
+        };
 
-        Main.main(dbDoc);
+        String[] diff = {
+                "--url=jdbc:mysql://localhost/task_manager1",
+                "--username=root",
+                "--password=",
+                "--outputFile=./dbDoc/diff.sql",
+                "--referenceUrl=jdbc:mysql://localhost/task_manager",
+                "--referenceUsername=root",
+                "--referencePassword=",
+                "diff"
+        };
+
+        String[] update = {
+                "--changeLogFile=current-changelog.xml",
+                "--url=jdbc:mysql://localhost/task_manager",
+                "--username=root",
+                "--password=",
+                "--outputFile=./dbDoc/updateSql.sql",
+                "update"
+        };
+
+
+        Main.main(generateChangeLog);
+//        Main.main(generateChangeLog);
     }
 
 }
